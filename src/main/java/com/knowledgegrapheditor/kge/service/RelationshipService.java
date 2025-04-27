3c7f53eff95f1ccfaa08652dfd7d378c9c65bf08
JanaAngjelkoska <jana.angjelkoska@hotmail.com>
2025-04-27 12:41:40 +0200

    Relationship service implementation

--- src/main/java/com/knowledgegrapheditor/kge/service/RelationshipService.java
+++ src/main/java/com/knowledgegrapheditor/kge/service/RelationshipService.java
@@ -1,9 +1,18 @@
 package com.knowledgegrapheditor.kge.service;
-
-import com.knowledgegrapheditor.kge.model.NodeDTO;
 import com.knowledgegrapheditor.kge.model.RelationshipDTO;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface RelationshipService {
 
     Iterable<RelationshipDTO> findAll();
+    Optional<RelationshipDTO> findById(UUID id);
+    Optional<RelationshipDTO> findBySourceAndDestinationNodeId(UUID sourceId, UUID destinationId);
+    RelationshipDTO create(UUID sourceNodeId, UUID destinationNodeId, String relationshipLabel, Map<String, Object> parameters);
+    Optional<RelationshipDTO> modifyLabelOf(UUID sourceNodeId, UUID destinationNodeId, String newLabel);
+    boolean deleteBySourceAndDestinationNodeId(UUID sourceId, UUID destinationId);
+    boolean deleteById(UUID relationshipId);
+
 }
--- src/main/java/com/knowledgegrapheditor/kge/service/impl/RelationshipServiceImpl.java
+++ src/main/java/com/knowledgegrapheditor/kge/service/impl/RelationshipServiceImpl.java
@@ -3,7 +3,12 @@ package com.knowledgegrapheditor.kge.service.impl;
 import com.knowledgegrapheditor.kge.model.RelationshipDTO;
 import com.knowledgegrapheditor.kge.repository.ArbitraryRelationshipRepository;
 import com.knowledgegrapheditor.kge.service.RelationshipService;
+import org.springframework.stereotype.Service;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+@Service
 public class RelationshipServiceImpl implements RelationshipService {
 
     private final ArbitraryRelationshipRepository relationshipRepository;
@@ -15,5 +20,35 @@ public class RelationshipServiceImpl implements RelationshipService {
     @Override
     public Iterable<RelationshipDTO> findAll() {
         return relationshipRepository.findAll();
+    }
+
+    @Override
+    public Optional<RelationshipDTO> findById(UUID id) {
+        return relationshipRepository.findById(id);
+    }
+
+    @Override
+    public Optional<RelationshipDTO> findBySourceAndDestinationNodeId(UUID sourceId, UUID destinationId) {
+        return relationshipRepository.findBySourceAndDestinationNodeId(sourceId, destinationId);
+    }
+
+    @Override
+    public RelationshipDTO create(UUID sourceNodeId, UUID destinationNodeId, String relationshipLabel, Map<String, Object> parameters) {
+        return relationshipRepository.create(sourceNodeId, destinationNodeId, relationshipLabel, parameters);
+    }
+
+    @Override
+    public Optional<RelationshipDTO> modifyLabelOf(UUID sourceNodeId, UUID destinationNodeId, String newLabel) {
+        return relationshipRepository.modifyLabelOf(sourceNodeId, destinationNodeId, newLabel);
+    }
+
+    @Override
+    public boolean deleteBySourceAndDestinationNodeId(UUID sourceId, UUID destinationId) {
+        return relationshipRepository.deleteBySourceAndDestinationNodeId(sourceId, destinationId);
+    }
+
+    @Override
+    public boolean deleteById(UUID relationshipId) {
+        return relationshipRepository.deleteById(relationshipId);
     }
 }
