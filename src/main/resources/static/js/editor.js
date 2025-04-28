aa254f791b267eea11638fab46d2347820731462
David Dukoski <duko_7@outlook.com>
2025-04-28 12:22:28 +0200

    feature: Add pastel color mapping.

--- src/main/resources/static/js/editor.js
+++ src/main/resources/static/js/editor.js
@@ -13,8 +13,8 @@ const labelColorMap = {}; // stores assigned colors
 
 function randomColor() {
     const hue = Math.floor(Math.random() * 360);
-    const saturation = Math.floor(Math.random() * 40) + 30;
-    const lightness = Math.floor(Math.random() * 30) + 60;
+    const saturation = Math.floor(Math.random() * 20) + 20;
+    const lightness = Math.floor(Math.random() * 20) + 70;
     return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
 }
 
